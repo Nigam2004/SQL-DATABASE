8d4c0bcf89ba2e407ede619875e7e7943a583750
Nigam Bisoyi <nigambisoyi2004@gmail.com>
2026-07-26 14:20:57 +0530

    DML_sql

---
+++ DML(DATA MANIPULATION LANGUAGE).sql
@@ -0,0 +1,13 @@
+-- INSERT THE DATA INTO TABLE 
+insert into emp_data(emp_name,job_name,j_date,city,salary,dept_no)
+values 
+('nigam', 'software', '2025-10-25', 'bbsr', 25000, 10),
+('raj', 'dataanalyst', '2025-10-25', 'klkt', 26000, 20),
+('suman', 'finance', '2025-10-25', 'dlhi', 28000, 10),
+('depak', 'hr', '2025-10-25', 'bglr', 24000, 20),
+('rahul', 'executive', '2025-10-25', 'hyd', 30000, 10);
+
+update emp_data set emp_name='sneha' where city='hyd'; --update the filed in a record
+delete from emp_data where emp_name='raj'; --delete the record
+
+select * from emp_data;
